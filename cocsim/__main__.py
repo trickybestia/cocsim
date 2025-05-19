af2242163adf96c79936c12136dbb2d366036b1f
trickybestia <trickybestia@gmail.com>
2025-05-19 03:22:25 +0300

    Draw drop zone

--- cocsim/__main__.py
+++ cocsim/__main__.py
@@ -1,5 +1,7 @@
 import pygame
 
+from .tile_color import get_tile_color
+
 BASE_WIDTH = 44
 BASE_HEIGHT = 44
 MAP_BORDERS = 3
@@ -9,35 +11,46 @@ MAP_HEIGHT = BASE_HEIGHT + MAP_BORDERS * 2
 
 PIXELS_PER_CELL = 20
 
-CELL_COLOR = pygame.color.Color(40, 40, 40)
-CELL_INVERTED_COLOR = pygame.color.Color(10, 10, 10)
-BORDER_CELL_COLOR = pygame.color.Color(30, 60, 0)
-BORDER_CELL_INVERTED_COLOR = pygame.color.Color(20, 30, 0)
 
+def compute_drop_zone(buildings: list[list[bool]]) -> list[list[bool]]:
+    def get_neighbors(tile_x: int, tile_y: int) -> list[tuple[int, int]]:
+        result = []
 
-def draw_grid(screen: pygame.Surface):
+        for neighbor_x in range(tile_x - 1, tile_x + 2):
+            for neighbor_y in range(tile_y - 1, tile_y + 2):
+                if 0 <= neighbor_x < MAP_WIDTH and 0 <= neighbor_y < MAP_HEIGHT:
+                    result.append((neighbor_x, neighbor_y))
+
+        return result
+
+    result = [[True] * MAP_HEIGHT for _ in range(MAP_WIDTH)]
+
     for tile_x in range(MAP_WIDTH):
         for tile_y in range(MAP_HEIGHT):
-            border_cell = (
+            if buildings[tile_x][tile_y]:
+                for neighbor_x, neighbor_y in get_neighbors(tile_x, tile_y):
+                    result[neighbor_x][neighbor_y] = False
+
+    return result
+
+
+def draw_grid(screen: pygame.Surface, drop_zone: list[list[bool]]):
+    for tile_x in range(MAP_WIDTH):
+        for tile_y in range(MAP_HEIGHT):
+            is_border = (
                 tile_y < MAP_BORDERS
                 or tile_x < MAP_BORDERS
                 or tile_y >= BASE_HEIGHT + MAP_BORDERS
                 or tile_x >= BASE_WIDTH + MAP_BORDERS
             )
-            invert_color = (tile_y ^ tile_x) & 1
-
-            if border_cell:
-                fill_color = (
-                    BORDER_CELL_INVERTED_COLOR
-                    if invert_color
-                    else BORDER_CELL_COLOR
-                )
-            else:
-                fill_color = CELL_INVERTED_COLOR if invert_color else CELL_COLOR
 
             pygame.draw.rect(
                 screen,
-                fill_color,
+                get_tile_color(
+                    (tile_y ^ tile_x) & 1,
+                    is_border,
+                    drop_zone[tile_x][tile_y],
+                ),
                 (
                     tile_x * PIXELS_PER_CELL,
                     tile_y * PIXELS_PER_CELL,
@@ -45,8 +58,6 @@ def draw_grid(screen: pygame.Surface):
                     PIXELS_PER_CELL,
                 ),
             )
-
-            invert_color = not invert_color
 
 
 def main():
@@ -56,7 +67,12 @@ def main():
         (MAP_WIDTH * PIXELS_PER_CELL, MAP_HEIGHT * PIXELS_PER_CELL)
     )
 
-    draw_grid(screen)
+    buildings = [[False] * MAP_HEIGHT for _ in range(MAP_WIDTH)]
+    buildings[10][10] = True
+
+    drop_zone = compute_drop_zone(buildings)
+
+    draw_grid(screen, drop_zone)
 
     pygame.display.update()
 
