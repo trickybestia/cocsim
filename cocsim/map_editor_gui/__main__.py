4bd2a5d1f8d3bb41efe37210add4f893bed9c5d0
trickybestia <trickybestia@gmail.com>
2025-05-21 18:19:21 +0300

    Implement creating square base image. Find out that game randomizes edge camera position or zoom level every time!! 😠

--- cocsim/map_editor_gui/__main__.py
+++ cocsim/map_editor_gui/__main__.py
@@ -1,8 +1,17 @@
+from io import BytesIO
+
 import PIL
 import PIL.Image
+import PIL.ImageDraw
+import wand.image
+
+from cocsim.consts import *
 
 
-def concat_base_images(
+SQUARE_IMAGE_SIZE = 832
+
+
+def compose_base_images(
     top: PIL.Image.Image, bottom: PIL.Image.Image
 ) -> PIL.Image.Image:
     BOTTOM_CROP_Y = 236
@@ -20,11 +29,71 @@ def concat_base_images(
     return composed
 
 
+def reverse_projection(image: PIL.Image.Image) -> PIL.Image.Image:
+    TOP_CORNER_POS = (1220, 41)
+    BOTTOM_CORNER_POS = (1221, 1536)
+    LEFT_CORNER_POS = (222, 788)
+
+    image_stream = BytesIO()
+
+    image.save(image_stream, "bmp")
+
+    with wand.image.Image(blob=image_stream.getvalue()) as wand_image:
+        wand_image.artifacts["distort:viewport"] = (
+            f"{SQUARE_IMAGE_SIZE}x{SQUARE_IMAGE_SIZE}+0+0"
+        )
+        wand_image.distort(
+            "affine",
+            (
+                TOP_CORNER_POS[0],
+                TOP_CORNER_POS[1],
+                SQUARE_IMAGE_SIZE,
+                0,
+                BOTTOM_CORNER_POS[0],
+                BOTTOM_CORNER_POS[1],
+                0,
+                SQUARE_IMAGE_SIZE,
+                LEFT_CORNER_POS[0],
+                LEFT_CORNER_POS[1],
+                0,
+                0,
+            ),
+        )
+
+        return PIL.Image.open(BytesIO(wand_image.make_blob("bmp")))
+
+
+def draw_grid(image: PIL.Image.Image):
+    pixels_per_tile = SQUARE_IMAGE_SIZE // MAP_WIDTH
+
+    draw = PIL.ImageDraw.ImageDraw(image)
+
+    for x in range(MAP_WIDTH):
+        for y in range(MAP_HEIGHT):
+            draw.rectangle(
+                (
+                    (x * pixels_per_tile, y * pixels_per_tile),
+                    (
+                        (x + 1) * pixels_per_tile - 1,
+                        (y + 1) * pixels_per_tile - 1,
+                    ),
+                ),
+                outline="black",
+            )
+
+
 def main():
     top = PIL.Image.open("top.jpg")
     bottom = PIL.Image.open("bottom.jpg")
 
-    concat_base_images(top, bottom).show()
+    composed = compose_base_images(top, bottom)
+    square = reverse_projection(composed)
+
+    draw_grid(
+        square,
+    )
+
+    square.show()
 
 
 main()
