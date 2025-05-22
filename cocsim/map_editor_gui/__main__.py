81d43b1ad2c94ed5fb6519347b1529d3cd150fa0
trickybestia <trickybestia@gmail.com>
2025-05-22 09:18:28 +0300

    Change image similarity score formula

--- cocsim/map_editor_gui/__main__.py
+++ cocsim/map_editor_gui/__main__.py
@@ -8,16 +8,54 @@ import wand.image
 import numpy as np
 
 from cocsim.consts import *
-from .compose_base_images import compose_base_images
 
 
 SQUARE_IMAGE_SIZE = 832
+
+
+def remove_vignette(image: PIL.Image.Image):
+    VIGNETTE_SIZE = 200
+
+    for y in range(VIGNETTE_SIZE):
+        r_offset = (
+            9.340e-06 * y**3 - 2.450e-03 * y**2 - 4.349e-02 * y + 3.862e01
+        )
+        g_offset = (
+            1.106e-05 * y**3 - 3.010e-03 * y**2 - 3.807e-02 * y + 4.894e01
+        )
+        b_offset = (
+            3.450e-06 * y**3 - 1.006e-03 * y**2 - 2.616e-02 * y + 2.091e01
+        )
+
+        if y >= 180:
+            K = (200 - y) / (200 - 180)
+            r_offset *= K
+            g_offset *= K
+            b_offset *= K
+
+        r_offset = int(r_offset)
+        g_offset = int(g_offset)
+        b_offset = int(b_offset)
+
+        for x in range(image.width):
+            r, g, b = image.getpixel((x, y))
+
+            image.putpixel(
+                (x, y),
+                (
+                    r + r_offset,
+                    g + g_offset,
+                    b + b_offset,
+                ),
+            )
 
 
 def compose_base_images(
     top: PIL.Image.Image, bottom: PIL.Image.Image
 ) -> PIL.Image.Image:
     BOTTOM_CROP_Y = 0  # vignette end
+
+    remove_vignette(bottom)
 
     bottom = bottom.crop((0, BOTTOM_CROP_Y, bottom.width, bottom.height))
 
@@ -41,13 +79,15 @@ def find_tear_line(top: PIL.Image.Image, bottom: PIL.Image.Image) -> int:
         image = PIL.ImageChops.subtract(
             top.crop((0, y, top.width, top.height)), bottom
         )
-        difference = np.asarray(image, dtype="int32").sum()
+        difference = np.asarray(image, dtype="int32").sum() / image.size[1]
 
         if smallest_difference is None or difference < smallest_difference:
             smallest_difference_y = y
             smallest_difference = difference
 
-        print(y, difference)
+        print(y, image.size, difference)
+
+    print(smallest_difference_y, smallest_difference)
 
     return smallest_difference_y
 
@@ -106,8 +146,8 @@ def draw_grid(image: PIL.Image.Image):
 
 
 def main():
-    top = PIL.Image.open("compose_base_images_dataset/top3.jpg")
-    bottom = PIL.Image.open("compose_base_images_dataset/bottom3.jpg")
+    top = PIL.Image.open("compose_base_images_dataset/top2.jpg")
+    bottom = PIL.Image.open("compose_base_images_dataset/bottom2.jpg")
 
     composed = compose_base_images(top, bottom)
 
