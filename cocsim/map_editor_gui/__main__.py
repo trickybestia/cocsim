2783149d1f52814e03d8c629e70fd04c80c25008
trickybestia <trickybestia@gmail.com>
2025-05-22 07:45:37 +0300

    tmp

--- cocsim/map_editor_gui/__main__.py
+++ cocsim/map_editor_gui/__main__.py
@@ -2,14 +2,54 @@ from io import BytesIO
 
 import PIL
 import PIL.Image
+import PIL.ImageChops
 import PIL.ImageDraw
 import wand.image
+import numpy as np
 
 from cocsim.consts import *
 from .compose_base_images import compose_base_images
 
 
 SQUARE_IMAGE_SIZE = 832
+
+
+def compose_base_images(
+    top: PIL.Image.Image, bottom: PIL.Image.Image
+) -> PIL.Image.Image:
+    BOTTOM_CROP_Y = 0  # vignette end
+
+    bottom = bottom.crop((0, BOTTOM_CROP_Y, bottom.width, bottom.height))
+
+    bottom_paste_y = find_tear_line(top, bottom) + 1
+
+    composed = PIL.Image.new(
+        "RGB", (top.width, bottom_paste_y - BOTTOM_CROP_Y + bottom.height)
+    )
+
+    composed.paste(top, (0, 0))
+    composed.paste(bottom, (0, bottom_paste_y))
+
+    return composed
+
+
+def find_tear_line(top: PIL.Image.Image, bottom: PIL.Image.Image) -> int:
+    smallest_difference = None
+    smallest_difference_y = None
+
+    for y in range(top.height // 2, int(top.height * 0.8)):
+        image = PIL.ImageChops.subtract(
+            top.crop((0, y, top.width, top.height)), bottom
+        )
+        difference = np.asarray(image, dtype="int32").sum()
+
+        if smallest_difference is None or difference < smallest_difference:
+            smallest_difference_y = y
+            smallest_difference = difference
+
+        print(y, difference)
+
+    return smallest_difference_y
 
 
 def reverse_projection(image: PIL.Image.Image) -> PIL.Image.Image:
@@ -66,8 +106,8 @@ def draw_grid(image: PIL.Image.Image):
 
 
 def main():
-    top = PIL.Image.open("compose_base_images_dataset/top0.1.jpg")
-    bottom = PIL.Image.open("compose_base_images_dataset/bottom0.1.jpg")
+    top = PIL.Image.open("compose_base_images_dataset/top3.jpg")
+    bottom = PIL.Image.open("compose_base_images_dataset/bottom3.jpg")
 
     composed = compose_base_images(top, bottom)
 
