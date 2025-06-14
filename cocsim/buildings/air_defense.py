11679588569d27da087ba31f505ee159f61250b4
trickybestia <trickybestia@gmail.com>
2025-06-14 21:12:53 +0300

    Make AirDefense fire

--- cocsim/buildings/air_defense.py
+++ cocsim/buildings/air_defense.py
@@ -1,27 +1,38 @@
+from dataclasses import dataclass
+from typing import Type
+
 from .building import BUILDINGS
-from .active_building import ActiveBuilding
-from .. import game
+from .projectile_active_building import ProjectileActiveBuilding
+from .. import game, units
 from .colliders import RectCollider
 
 
-class AirDefense(ActiveBuilding):
-    HEALTH = [
-        800.0,
-        850.0,
-        900.0,
-        950.0,
-        1000.0,
-        1050.0,
-        1100.0,
-        1210.0,
-        1300.0,
-        1400.0,
-        1500.0,
-        1650.0,
-        1750.0,
-        1850.0,
-        1950.0,
-    ]
+@dataclass(frozen=True)
+class AirDefenseLevel:
+    health: float
+    attack_damage: float
+
+
+class AirDefense(ProjectileActiveBuilding):
+    LEVELS = (
+        AirDefenseLevel(800.0, 80.0),
+        AirDefenseLevel(850.0, 110.0),
+        AirDefenseLevel(900.0, 140.0),
+        AirDefenseLevel(950.0, 160.0),
+        AirDefenseLevel(1000.0, 190.0),
+        AirDefenseLevel(1050.0, 230.0),
+        AirDefenseLevel(1100.0, 280.0),
+        AirDefenseLevel(1210.0, 320.0),
+        AirDefenseLevel(1300.0, 360.0),
+        AirDefenseLevel(1400.0, 400.0),
+        AirDefenseLevel(1500.0, 440.0),
+        AirDefenseLevel(1650.0, 500.0),
+        AirDefenseLevel(1750.0, 540.0),
+        AirDefenseLevel(1850.0, 600.0),
+        AirDefenseLevel(1950.0, 650.0),
+    )
+
+    level: int
 
     @classmethod
     def width(cls) -> int:
@@ -33,14 +44,33 @@ class AirDefense(ActiveBuilding):
 
     @classmethod
     def levels(cls) -> int:
-        return len(cls.HEALTH)
+        return len(cls.LEVELS)
+
+    @classmethod
+    def attack_range(cls) -> float:
+        return 10.0
+
+    @classmethod
+    def attack_cooldown(cls) -> float:
+        return 1.0
+
+    @classmethod
+    def projectile_speed(cls) -> float:
+        return 10.7
+
+    @classmethod
+    def target_type(cls) -> Type["units.Unit"] | None:
+        return units.AirUnit
+
+    def attack_damage(self):
+        return self.LEVELS[self.level].attack_damage
 
     def __init__(self, game: "game.Game", x: float, y: float, level: int):
         super().__init__(
             game,
             x,
             y,
-            self.HEALTH[level],
+            self.LEVELS[level].health,
             RectCollider.from_center(
                 x + self.width() / 2,
                 y + self.height() / 2,
@@ -48,6 +78,8 @@ class AirDefense(ActiveBuilding):
                 self.height() * 0.65,
             ),
         )
+
+        self.level = level
 
 
 BUILDINGS.append(AirDefense)
