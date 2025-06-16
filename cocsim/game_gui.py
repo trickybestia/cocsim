d797d5fb4dbceff48107ca8882972796560d5ae8
trickybestia <trickybestia@gmail.com>
2025-06-16 21:22:53 +0300

    Add GameGui

---
+++ cocsim/game_gui.py
@@ -0,0 +1,53 @@
+import pygame
+
+from cocsim.spin_timer import SpinTimer
+from cocsim.consts import *
+from cocsim.game import Game
+
+
+class GameGui:
+    game: Game
+
+    def __init__(
+        self,
+        game: Game,
+    ):
+        self.game = game
+
+    def run(self):
+        pygame.init()
+        pygame.display.set_caption("cocsim")
+
+        screen = pygame.display.set_mode(
+            (
+                self.game.total_size * PIXELS_PER_TILE,
+                self.game.total_size * PIXELS_PER_TILE,
+            )
+        )
+
+        self.game.screen = screen
+
+        timer = SpinTimer(1 / FPS)
+
+        def on_tick(delta_t: float | None):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    timer.stop()
+
+                    return
+
+            self.game.draw()
+
+            if not self.game.done and delta_t is not None:
+                delta_t = min(delta_t, 2 * 1 / FPS)
+                self.game.tick(delta_t)
+                pygame.display.set_caption(
+                    f"cocsim | {self.game.progress_info()}"
+                )
+
+            pygame.display.update()
+
+        timer.on_tick = on_tick
+
+        timer.run()
--- cocsim/showcase/__main__.py
+++ cocsim/showcase/__main__.py
@@ -1,25 +1,13 @@
-import pygame
-
 from cocsim.utils import load_test_map
-from cocsim.spin_timer import SpinTimer
+from cocsim.game_gui import GameGui
 from cocsim.units import Dragon, Barbarian
-from cocsim.consts import *
 from cocsim.game import Game
 
 
 def main():
-    pygame.init()
-    pygame.display.set_caption("cocsim")
-
     map, base_image = load_test_map("single_player/goblin_gauntlet")
 
     game = Game(map, base_image)
-
-    screen = pygame.display.set_mode(
-        (game.total_size * PIXELS_PER_TILE, game.total_size * PIXELS_PER_TILE)
-    )
-
-    game.screen = screen
 
     game.units = []
 
@@ -28,27 +16,9 @@ def main():
     for x in range(1, 25):
         game.units.append(Barbarian(game, 1, x + 0.5, 0.5))
 
-    timer = SpinTimer(1 / FPS)
+    gui = GameGui(game)
 
-    def on_tick(delta_t: float | None):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                timer.stop()
-
-                return
-
-        game.draw()
-
-        if not game.done and delta_t is not None:
-            delta_t = min(delta_t, 2 * 1 / FPS)
-            game.tick(delta_t)
-            pygame.display.set_caption(f"cocsim | {game.progress_info()}")
-
-        pygame.display.update()
-
-    timer.on_tick = on_tick
-
-    timer.run()
+    gui.run()
 
 
 main()
