f73e2863e7ae9a137169272495db8547bf86aa50
trickybestia <trickybestia@gmail.com>
2025-06-19 11:22:20 +0300

    Add DarkElixirDrill

---
+++ cocsim/buildings/dark_elixir_drill.py
@@ -0,0 +1,48 @@
+from .building import BUILDINGS
+from .passive_building import PassiveBuilding
+from .. import game
+from .colliders import RectCollider
+
+
+class DarkElixirDrill(PassiveBuilding):
+    HEALTH = [
+        800.0,
+        860.0,
+        920.0,
+        980.0,
+        1060.0,
+        1160.0,
+        1280.0,
+        1380.0,
+        1480.0,
+        1550.0,
+    ]
+
+    @classmethod
+    def width(cls) -> int:
+        return 3
+
+    @classmethod
+    def height(cls) -> int:
+        return 3
+
+    @classmethod
+    def levels(cls) -> int:
+        return len(cls.HEALTH)
+
+    def __init__(self, game: "game.Game", x: float, y: float, level: int):
+        super().__init__(
+            game,
+            x,
+            y,
+            self.HEALTH[level],
+            RectCollider.from_center(
+                x + self.width() / 2,
+                y + self.height() / 2,
+                self.width() * 0.65,
+                self.height() * 0.65,
+            ),
+        )
+
+
+BUILDINGS.append(DarkElixirDrill)
