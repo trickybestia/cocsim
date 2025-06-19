27f6b851e1f642c55e13939a30185dfe8c44563b
trickybestia <trickybestia@gmail.com>
2025-06-19 23:19:17 +0300

    Add SCSS

--- frontend/src/components/app/index.tsx
+++ frontend/src/components/app/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import "./index.scss";
+import reactLogo from "./react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
-import "./index.css";
+import App from "./components/app/index.tsx";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
