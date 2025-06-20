b975b60dfbd6114ad3d8255935073a1ffccebb6b
trickybestia <trickybestia@gmail.com>
2025-06-20 11:07:33 +0300

    Remove boilerplate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
+import "purecss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./components/app/index.tsx";
+import App from "./components/app";
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
