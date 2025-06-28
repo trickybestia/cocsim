18b601cda30745703bd4e299a1cb3fde2351a0cc
trickybestia <trickybestia@gmail.com>
2025-06-28 22:17:50 +0300

    Add shadow to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "wouter";
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 left-0 z-30 w-full bg-blue-400 p-1">
+    <header className="sticky top-0 left-0 z-30 w-full bg-blue-400 p-1 shadow-md">
       <Link
         className="cursor-pointer px-2 py-1 text-lg font-bold text-white"
         href="/"
