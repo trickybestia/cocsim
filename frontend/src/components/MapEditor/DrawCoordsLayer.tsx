d841224047fc3b0023d4f22c56deed4991b0be4c
trickybestia <trickybestia@gmail.com>
2025-06-29 23:59:08 +0300

    Use memoization for DrawGridLayer and DrawCoordsLayer

--- frontend/src/components/MapEditor/DrawCoordsLayer.tsx
+++ frontend/src/components/MapEditor/DrawCoordsLayer.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Layer, Text } from "react-konva";
 
 type Props = { totalSize: number; canvasSize: number };
@@ -7,34 +8,36 @@ const TEXT_OFFSET_X = -0.1;
 const TEXT_OFFSET_Y = -0.4;
 const TEXT_SIZE = 2;
 
-const DrawCoordsLayer: React.FC<Props> = ({ totalSize, canvasSize }: Props) => {
-  const pixelsPerTile = canvasSize / totalSize;
+const DrawCoordsLayer: React.FC<Props> = memo(
+  ({ totalSize, canvasSize }: Props) => {
+    const pixelsPerTile = canvasSize / totalSize;
 
-  const coords = [];
+    const coords = [];
 
-  for (let tile_x = 0; tile_x != totalSize; tile_x++) {
-    for (let tile_y = 0; tile_y != totalSize; tile_y++) {
-      const text = `${tile_x},${tile_y}`;
+    for (let tile_x = 0; tile_x != totalSize; tile_x++) {
+      for (let tile_y = 0; tile_y != totalSize; tile_y++) {
+        const text = `${tile_x},${tile_y}`;
 
-      coords.push(
-        <Text
-          key={text}
-          x={(tile_x + TEXT_OFFSET_X) * pixelsPerTile}
-          y={(tile_y + TEXT_OFFSET_Y) * pixelsPerTile}
-          width={TEXT_SIZE * pixelsPerTile}
-          height={TEXT_SIZE * pixelsPerTile}
-          align="center"
-          verticalAlign="center"
-          rotation={45}
-          fontSize={10}
-          text={text}
-        />
-      );
+        coords.push(
+          <Text
+            key={text}
+            x={(tile_x + TEXT_OFFSET_X) * pixelsPerTile}
+            y={(tile_y + TEXT_OFFSET_Y) * pixelsPerTile}
+            width={TEXT_SIZE * pixelsPerTile}
+            height={TEXT_SIZE * pixelsPerTile}
+            align="center"
+            verticalAlign="center"
+            rotation={45}
+            fontSize={10}
+            text={text}
+          />
+        );
+      }
     }
-  }
 
-  return <Layer>{coords}</Layer>;
-};
+    return <Layer>{coords}</Layer>;
+  }
+);
 
 export default DrawCoordsLayer;
 export type { Props };
--- frontend/src/components/MapEditor/DrawGridLayer.tsx
+++ frontend/src/components/MapEditor/DrawGridLayer.tsx
@@ -1,28 +1,31 @@
+import { memo } from "react";
 import { Circle, Layer } from "react-konva";
 
 type Props = { totalSize: number; canvasSize: number };
 
-const DrawGridLayer: React.FC<Props> = ({ totalSize, canvasSize }: Props) => {
-  const pixelsPerTile = canvasSize / totalSize;
+const DrawGridLayer: React.FC<Props> = memo(
+  ({ totalSize, canvasSize }: Props) => {
+    const pixelsPerTile = canvasSize / totalSize;
 
-  const dots = [];
+    const dots = [];
 
-  for (let tile_x = 0; tile_x != totalSize + 1; tile_x++) {
-    for (let tile_y = 0; tile_y != totalSize + 1; tile_y++) {
-      dots.push(
-        <Circle
-          key={`${tile_x}:${tile_y}`}
-          x={tile_x * pixelsPerTile}
-          y={tile_y * pixelsPerTile}
-          radius={2}
-          fill="white"
-        />
-      );
+    for (let tile_x = 0; tile_x != totalSize + 1; tile_x++) {
+      for (let tile_y = 0; tile_y != totalSize + 1; tile_y++) {
+        dots.push(
+          <Circle
+            key={`${tile_x}:${tile_y}`}
+            x={tile_x * pixelsPerTile}
+            y={tile_y * pixelsPerTile}
+            radius={2}
+            fill="white"
+          />
+        );
+      }
     }
-  }
 
-  return <Layer>{dots}</Layer>;
-};
+    return <Layer>{dots}</Layer>;
+  }
+);
 
 export default DrawGridLayer;
 export type { Props };
