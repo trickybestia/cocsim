55be1025094db23f9671de5aad16212fac472162
trickybestia <trickybestia@gmail.com>
2025-08-30 22:08:36 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss(), topLevelAwait()],
   worker: {
     format: "es"
